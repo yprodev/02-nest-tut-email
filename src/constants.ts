a5bc619db4b963a06c7d921ce2869380ef87c9f9
Yaroslav Prodanchuk <yaroslav.prodanchuk.external@ryd.one>
2022-06-04 14:46:42 +0300

    Configure microservice

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const ENV_VARS_NAMES = {
+  PORT: 'PORT',
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
 import { AppModule } from './app.module';
+import { ENV_VARS_NAMES as E } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  await app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      port: configService.get(E.PORT),
+    },
+  });
+
+  app.startAllMicroservices();
 }
 bootstrap();
